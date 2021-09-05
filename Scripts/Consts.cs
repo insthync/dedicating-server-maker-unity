f18b44fa8bab481aa3b6a6d7bd06f0bf62809d4c
Insthync <ittipon.bay@gmail.com>
2021-09-05 09:16:04 +0700

    Add consts

---
+++ Scripts/Consts.cs
@@ -0,0 +1,10 @@
+namespace DedicatingServerMatchMaker
+{
+    public static class Consts
+    {
+        public const string OPTION_TITLE = "title";
+        public const string OPTION_PASSWORD = "password";
+        public const string ROOM_NAME = "lobby";
+        public const int MAX_PASSWORD_LENGTH = 10;
+    }
+}
