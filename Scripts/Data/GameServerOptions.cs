b3ad6c25b6006fbcc75f898221f08cec5742a4d4
Insthync <ittipon.bay@gmail.com>
2021-09-05 09:14:51 +0700

    Add game server options

---
+++ Scripts/Data/GameServerOptions.cs
@@ -0,0 +1,9 @@
+namespace DedicatingServerMatchMaker.Data
+{
+    [System.Serializable]
+    public partial class GameServerOptions
+    {
+        public string serverAddress = string.Empty;
+        public int serverPort = 0;
+    }
+}
